f260d4d81cff6fa7de5e3860b21ff9faabd33954
Jamie Jenks <jamielynjenks@gmail.com>
2022-12-23 00:47:19 -0700

    Initial commit & Setting up components folder & css reset

---
+++ src/Components/home.js
@@ -0,0 +1,14 @@
+import '../App.css';
+
+function Home() {
+  return (
+    <div className="Home">
+      <h1>Jamie Jenks</h1>
+      <h3>Full</h3>
+      <h3>Stack</h3>
+      <h3>Web</h3>
+    </div>
+  );
+}
+
+export default Home;
