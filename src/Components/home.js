27825c4c5b8cb9d0c3595b8fffd6efd075ff3397
Jamie Jenks <jamielynjenks@gmail.com>
2022-12-23 01:26:15 -0700

    neon heading start

--- src/Components/home.js
+++ src/Components/home.js
@@ -1,12 +1,12 @@
-import '../App.css';
+import './home.css';
 
 function Home() {
   return (
     <div className="Home">
-      <h1>Jamie Jenks</h1>
-      <h3>Full</h3>
-      <h3>Stack</h3>
-      <h3>Web</h3>
+      <h1 className="neon pink">Jamie Jenks</h1>
+      <h3 className="neon blue flicker_1">Full</h3>
+      <h3 className="neon blue flicker_2">Stack</h3>
+      <h3 className="neon blue flicker_3">Web</h3>
     </div>
   );
 }
